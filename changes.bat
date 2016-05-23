518e6bdb94f0ec08343b964d71f24e83d0736179
miroslav.novak@hp.com <nomail@ni.com>
2016-05-23 13:10:55 +0200

    user story #1055: commit

---
+++ changes.bat
@@ -0,0 +1,7 @@
+echo "defect" >> dummy.txt 
+git commit -am "defect #1001: commit"
+echo "story" >> dummy.txt 
+git commit -am "user story #1001: commit"
+echo "sorry" >> dummy.txt 
+git commit -am "user story #1055: commit"
+git push origin master
