657c8f0038662c36aa03cbaa26c0d656ca11e9a7
miroslav.novak@hp.com <nomail@ni.com>
2016-05-26 09:16:17 +0200

    defect #1005: commit

--- changes.bat
+++ changes.bat
@@ -1,9 +1,9 @@
 echo "defect" >> dummy.txt 
-git commit -am "defect #1001: commit"
+git commit -am "defect #1005: commit"
 echo "effect" >> dummy.txt 
-git commit -am "defect #1002: commit"
+git commit -am "defect #1007: commit"
 echo "story" >> dummy.txt 
-git commit -am "user story #1001: commit"
+git commit -am "user story #1004: commit"
 echo "sorry" >> dummy.txt 
-git commit -am "user story #1055: commit"
+git commit -am "user story #1006: commit"
 git push origin master
