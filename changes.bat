2d6cfcdd642a3a78d41e6f43bda837258e58d22b
miroslav.novak@hp.com <nomail@ni.com>
2016-07-13 12:12:50 +0200

    user story #1004: adding tests

--- changes.bat
+++ changes.bat
@@ -1,8 +1,12 @@
 echo "defect" >> dummy.txt 
+echo.  >> src\test\java\training\account\UserServiceTest.java 
+echo.  >> src\test\java\training\account\AccountTest.java 
 git commit -am "defect #1005: commit"
 echo "effect" >> dummy.txt 
 git commit -am "defect #1007: commit"
 echo "defect" >> dummy.txt 
+echo.  >> src\test\java\training\account\UserServiceTest.java 
+echo.  >> src\test\java\training\account\AccountTest.java 
 git commit -am "defect #1061: commit"
 echo "effect" >> dummy.txt 
 git commit -am "defect #1062: commit"
@@ -12,16 +16,22 @@ git commit -am "defect #1062: commit"
 echo "story" >> dummy.txt 
 git commit -am "user story #1004: commit"
 echo "sorry" >> dummy.txt 
+echo.  >> src\test\java\training\account\UserServiceTest.java 
+echo.  >> src\test\java\training\account\AccountTest.java 
 git commit -am "user story #1006: commit"
 echo "story" >> dummy.txt 
 git commit -am "user story #1065: commit"
 echo "sorry" >> dummy.txt 
+echo.  >> src\test\java\training\account\UserServiceTest.java 
+echo.  >> src\test\java\training\account\AccountTest.java 
 git commit -am "user story #1066: commit"
 echo "story" >> dummy.txt 
 git commit -am "user story #1055: commit"
 echo "sorry" >> dummy.txt 
 git commit -am "user story #1056: commit"
 echo "story" >> dummy.txt 
+echo.  >> src\test\java\training\account\UserServiceTest.java 
+echo.  >> src\test\java\training\account\AccountTest.java 
 git commit -am "user story #1084: commit"
 echo "sorry" >> dummy.txt 
 git commit -am "user story #1085: commit"
--- src/test/java/training/account/AccountTest.java
+++ src/test/java/training/account/AccountTest.java
@@ -52,12 +52,57 @@ public class AccountTest {
         b = new Account("testu", "testp", "testr1");
         assertNotSame(a, b);
 
+        a = new Account("testu1", "testp", "testr");
+        b = new Account("testu", "testp", "testr1");
+        assertNotSame(a, b);
+    }
+
+    @Test
+    public void accountNotEqualsTest2() {
+        Account a = new Account("1testu", "testp", "testr");
+        Account b = new Account("testu", "testp", "testr");
+        assertNotSame(a, b);
+
+        a = new Account("testu", "testp", "testr");
+        b = new Account("testu", "testp1", "testr");
+        assertNotSame(a, b);
+
+        a = new Account("testu", "testp", "testr");
+        b = new Account("testu", "testp", "testr1");
+        assertNotSame(a, b);
+
 // story 1004
-if (true) throw new RuntimeException("This test is failing now");
+        if (true) throw new RuntimeException("This test is failing now");
 
         a = new Account("testu1", "testp", "testr");
         b = new Account("testu", "testp", "testr1");
         assertNotSame(a, b);
+    }
+
+    @Test
+    public void accountEquals2AccountsTest() {
+        Account a, b = null;
+        a = new Account("testua", "testp", "testr");
+        if (Math.random() > 0.5)
+            b = new Account("testu", "testp", "testr");
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void accountEqualsCornerCasesTest() {
+        Account a, b = null;
+        a = null;
+        if (Math.random() > 0.5)
+            b = new Account("testu", "testp", "testr");
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void accountToStringTest() {
+        Account a = null;
+        if (Math.random() > 0.5)
+            a = new Account("testu", "testp", "testr");
+        assertEquals("testu", a.toString());
     }
 
     @Test
