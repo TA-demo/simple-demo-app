2834a0c3ae1d06a36fffaf62ac6ed641f6cb830d
miroslav.novak@hp.com <nomail@ni.com>
2016-07-14 08:52:13 +0200

    defect #1005: minor fix

--- changes.bat
+++ changes.bat
@@ -5,7 +5,25 @@ git commit -am "defect #1007: commit"
 echo "defect" >> dummy.txt 
 git commit -am "defect #1061: commit"
 echo "effect" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
 git commit -am "defect #1062: commit"
+echo "effect" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+git commit -am "defect #1016: commit"
+echo "effect" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+git commit -am "defect #1025: commit"
+echo "effect" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+git commit -am "defect #1026: commit"
 
 
 
@@ -18,13 +36,29 @@ git commit -am "user story #1065: commit"
 echo "sorry" >> dummy.txt 
 git commit -am "user story #1066: commit"
 echo "story" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
 git commit -am "user story #1055: commit"
 echo "sorry" >> dummy.txt 
 git commit -am "user story #1056: commit"
 echo "story" >> dummy.txt 
 git commit -am "user story #1084: commit"
 echo "sorry" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
 git commit -am "user story #1085: commit"
+echo "sorry" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+git commit -am "user story #1033: commit"
+echo "sorry" >> dummy.txt 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+git commit -am "user story #1034: commit"
 
 
 echo.  >> src\main\resources\spring-security-context.xml 
