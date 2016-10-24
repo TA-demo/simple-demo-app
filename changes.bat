2464d5476941841ffd721d52de645e95497e0b35
miroslav.novak@hp.com <billy@et.com>
2016-10-24 13:59:31 +0200

    defect #1005: fixing defect

--- changes.bat
+++ changes.bat
@@ -1,24 +1,61 @@
+git config --global user.name "Billy Everyteen"
+git config --global user.email "billy@et.com"
+
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "defect" >> dummy.txt 
 git commit -am "defect #1005: fixing defect"
 echo "effect" >> dummy.txt 
 git commit -am "defect #1007: fixing error handling"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "defect" >> dummy.txt 
+
+git config --global user.name "Marry Jonset"
+git config --global user.email "marry@et.com"
+
 git commit -am "defect #1061: fixing NPE"
 echo "effect" >> dummy.txt 
 echo.  >> src\main\resources\spring-security-context.xml 
 echo.  >> src\main\resources\persistence.properties 
 echo.  >> src\main\resources\logback.xml 
 git commit -am "defect #1018: simple fix"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "effect" >> dummy.txt 
 echo.  >> src\main\resources\spring-security-context.xml 
 echo.  >> src\main\resources\persistence.properties 
 echo.  >> src\main\resources\logback.xml 
 git commit -am "defect #1019: fixing problem with permissions"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "effect" >> dummy.txt 
 echo.  >> src\main\resources\spring-security-context.xml 
 echo.  >> src\main\resources\persistence.properties 
 echo.  >> src\main\resources\logback.xml 
 git commit -am "defect #1020: fixing unstable connection"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "effect" >> dummy.txt 
 echo.  >> src\main\resources\spring-security-context.xml 
 echo.  >> src\main\resources\persistence.properties 
@@ -26,23 +63,53 @@ echo.  >> src\main\resources\logback.xml
 git commit -am "defect #1021: problems with connection pool"
 
 
+git config --global user.name "Eric Korsan"
+git config --global user.email "eric@et.com"
 
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "story" >> dummy.txt 
 echo.  >> src\test\java\training\account\AccountTest.java
 echo.  >> src\test\java\training\account\UserServiceTest.java
 git commit -am "user story #1004: user can add more widgets"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo.  >> src\test\java\training\account\AccountTest.java
 echo.  >> src\test\java\training\account\UserServiceTest.java
 echo "sorry" >> dummy.txt
 git commit -am "user story #1006: improving user service"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo.  >> src\test\java\training\account\AccountTest.java
 echo.  >> src\test\java\training\account\UserServiceTest.java
 echo "story" >> dummy.txt
 git commit -am "user story #1036: adding acount check"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo.  >> src\test\java\training\account\AccountTest.java
 echo.  >> src\test\java\training\account\UserServiceTest.java
 echo "sorry" >> dummy.txt
 git commit -am "user story #1037: simplifying request hadnling"
+
+git config --global user.name "Jim Hartric"
+git config --global user.email "jim@et.com"
+
 echo.  >> src\test\java\training\account\AccountTest.java
 echo.  >> src\test\java\training\account\UserServiceTest.java
 echo "story" >> dummy.txt
@@ -50,8 +117,20 @@ echo.  >> src\main\resources\spring-security-context.xml
 echo.  >> src\main\resources\persistence.properties 
 echo.  >> src\main\resources\logback.xml 
 git commit -am "user story #1038: refactor of loopback"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "sorry" >> dummy.txt 
 git commit -am "user story #1051: widget configuration"
+echo.  >> src\main\resources\spring-security-context.xml 
+echo.  >> src\main\resources\persistence.properties 
+echo.  >> src\main\resources\logback.xml 
+echo.  >> src\main\webapp\WEB-INF\web.xml
+echo.  >> src\test\java\training\account\AccountTest.java
+echo.  >> src\test\java\training\account\UserServiceTest.java 
 echo "story" >> dummy.txt 
 git commit -am "user story #1352: log analysis"
 echo "sorry" >> dummy.txt 
